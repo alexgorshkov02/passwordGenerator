b1cf625d7b8e55d19d147f3b71581d6b206e1d97
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-05-08 01:16:13 -0400

    Added the feature for choosing password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Global variables
+var lengthPassword = null;
+
+
+// additional funtional
+function lengthChoice() {
+  var hint = " ";
+
+  while (hint) {
+    lengthPassword = window.prompt("Please provide the password length?" + hint);
+    // Parse the string to a number in the decimal mathematical system
+    lengthPassword = parseInt(lengthPassword, 10);
+    hint = "(The value must be a number at least 8 characters and no more than 128 characters)";
+
+    if (Number.isInteger(lengthPassword) && lengthPassword >= 8 && lengthPassword < 128) {
+      hint = false;
+    }
+  }
+}
+
+
+
+// main functional
+function generatePassword() {
+  lengthChoice();
+
+
+
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +44,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
